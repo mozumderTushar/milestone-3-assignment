32b4961db7867f1aeb95d10d677eabc834f18ddd
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-07-17 11:15:05 +0600

    Done

--- assignment.js
+++ assignment.js
@@ -1,45 +1,73 @@
-/**
 //feetToMile
 function feetToMile(feet){
     if(feet > 0){
-        var mile = feet / 5280;
+        var mile = (feet / 5280).toFixed(4);
         return mile;
     }else if(feet < 0){
-        return "mile can't be negative";
+        return "Please Give a positive value for feet.";
     }else if(feet == 0){
         return "You entered 0";
     }
 }
-var calcFeet = feetToMile(150);
-console.log(calcFeet); 
 
 
 //woodCalculator
 function woodCalculator(chair,table,bed){
-    var total = (chair * 1) + (table * 3) + (bed * 5);
-    return total;
+    if(chair <0 || table < 0 || bed < 0){
+        return "Please Give Positive Values.";
+    }
+    else{
+        var totalWood = (chair*1) + (table*3) + (bed*5);
+        return totalWood;
+    }
 }
-var totalWood = woodCalculator(14,4,2);
-console.log(totalWood);
- */
+
 
  //brickCalculator
+ function brickCalculator(floor){
+    if(floor<0){
+        return "Please Give Positive Value";
+    }
+    else if( floor>=0 && floor<=10){
+        bricksWithinTen = floor*15*1000;
+        return bricksWithinTen;
+    }
+    else if(floor>10 && floor<=20){
+        var firstTenFloor = 10;
+        var afterTenFloor = floor - firstTenFloor;
+        var bricksWithinTwenty = (firstTenFloor*15*1000)+(afterTenFloor*12*1000);
+        return bricksWithinTwenty;
+    }
+    else if(floor>20) {
+       var firstTenFloor = 10;
+       var secondTenFloor = 10;
+       var firstTwentyFloor = firstTenFloor + secondTenFloor;
+       var afterFirstTwentyFloor = floor - firstTwentyFloor;
+       var totalBricks = (firstTenFloor*15*1000)+(secondTenFloor*12*1000)+(afterFirstTwentyFloor*10*1000);
+       return totalBricks;
+        
+    }
+ }
 
- 
- 
+
+
  //tinyFriend
 function tinyFriend(name){
     var small = name[0];
-    for(var i=0; i<name.length; i++){
+    if(small.length == 0){
+        return "It cant be empty, Please add your friends name";
+    }
+    else{
+         for(var i=0; i<name.length; i++){
        var element = name[i];
        if(element.length < small.length){
            small = element;
+         }
        }
     }
-    return small
+   
+    return small;
 }
 
-var tinyName = tinyFriend(['tushar','aB','fahim','sohi','burhan','abc','mifta','sohi','cksfnssd','ab']);
-console.log(tinyName);
 
 
